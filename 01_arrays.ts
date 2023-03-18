c97ede9fb55360217c210bc190fb8fa1bb18fc47
Daniel Agg <daniel.agg@outlook.com>
2023-03-18 15:24:43 +0100

    is anagram

--- 01_arrays.ts
+++ 01_arrays.ts
@@ -22,4 +22,26 @@ console.log(containsDuplicate_idea1([1, 2, 3, 4, 4, 5]));
 console.log(containsDuplicate_idea2([1, 2, 3, 4, 5]));
 console.log(containsDuplicate_idea2([1, 2, 3, 4, 4, 5]));
 
-// ---
+console.log("// ------------");
+console.log("// ------------");
+
+function isAnagram_idea1(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
+  const splitS = s.split("").sort();
+  const splitT = t.split("").sort();
+
+  return splitS.every((value, index) => value === splitT[index]);
+}
+
+function isAnagram_idea2(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
+  return s.split("").sort().join() == t.split("").sort().join();
+}
+
+console.log(isAnagram_idea1("anagram", "nagaram"));
+console.log(isAnagram_idea1("rat", "car"));
+
+console.log(isAnagram_idea2("anagram", "nagaram"));
+console.log(isAnagram_idea2("rat", "car"));
